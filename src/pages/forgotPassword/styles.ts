316759ab8ce47a09f24b0ac082943e0d3d275b76
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-09-03 15:23:25 -0300

    Finalização do Layout da página 'ForgotPassword'

---
+++ src/pages/forgotPassword/styles.ts
@@ -0,0 +1,35 @@
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles<Theme>(theme =>
+    createStyles({
+        container: { padding: '0% 10% 0px' },
+
+        title: { fontSize: 40, fontWeight: 700 },
+
+        description: {
+            marginTop: 10,
+            paddingRight: '15%',
+            color: '#505050',
+            fontSize: '1.1rem',
+        },
+
+        form: {
+            padding: '40px 0px',
+            paddingRight: '15%',
+        },
+
+        rightSide: {
+            marginTop: '4%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+        },
+    })
+);
+
+export const inputStyle = {
+    paddingLeft: 10,
+    fontFamily: 'Roboto',
+};
+
+export default useStyles;

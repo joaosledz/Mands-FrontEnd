4b6796fb1a780248839708c98e22c8420d062b2b
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-09-03 15:21:18 -0300

    Ajustes na página de Login e componentização do estilo da página.

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,5 @@
 import React, { FormEvent } from 'react';
-import { makeStyles } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import Box from '@material-ui/core/Box';
 // import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid';
@@ -11,10 +11,11 @@ import ButtonBase from '@material-ui/core/ButtonBase';
 import { UserTie } from '@styled-icons/fa-solid';
 import { Lock } from '@styled-icons/material';
 
-import LogInButton from './components/logInButton';
+import AuthLayout from '../../layout/authLayout';
+import LogInButton from '../../components/authPagesButton';
 import CompanyButton from './components/companyButton';
+import useStyles, { inputStyle } from './styles';
 
-import logo from '../../assets/logo.svg';
 import googleIcon from '../../assets/companiesIcons/googleLogo.svg';
 import microsoftIcon from '../../assets/companiesIcons/microsoftLogo.svg';
 import appleIcon from '../../assets/companiesIcons/appleLogo.svg';
@@ -22,188 +23,128 @@ import appleIcon from '../../assets/companiesIcons/appleLogo.svg';
 const Login: React.FC = () => {
     const classes = useStyles();
 
-    const handleForgotPassword = (event: { preventDefault: () => void }) => {
-        event.preventDefault();
-        console.log('handleForgotPassword');
-    };
-
     const handleSubmit = (event: FormEvent) => {
         event.preventDefault();
         console.log('handleSubmit');
     };
 
     return (
-        <Grid container className={classes.container}>
-            <Grid item xs={12} sm={6}>
-                <img
-                    src={logo}
-                    alt="logo"
-                    style={{ width: '20%', minWidth: 130, height: 'auto' }}
-                />
-                <Box mt={4}>
-                    <Typography
-                        style={{ fontSize: 40, fontWeight: 700 }}
-                        color="primary"
-                    >
-                        Log in
-                    </Typography>
-                    <FormControl fullWidth className={classes.form}>
-                        <Box
-                            display="flex"
-                            flexDirection="column"
-                            marginTop={3}
+        <AuthLayout>
+            <Grid container className={classes.container}>
+                <Grid item xs={12} sm={6}>
+                    <Box mt={4}>
+                        <Typography
+                            style={{ fontSize: 40, fontWeight: 700 }}
+                            color="primary"
                         >
-                            <TextField
-                                id="outlined-basic"
-                                label="Email"
-                                variant="outlined"
-                                InputProps={{
-                                    startAdornment: (
-                                        <UserTie size="20" color="#B03E9F" />
-                                    ),
-                                }}
-                                inputProps={{
-                                    style: inputStyle,
-                                }}
-                            />
-                            <TextField
-                                id="outlined-basic"
-                                type="password"
-                                label="Senha"
-                                variant="outlined"
-                                className={classes.input}
-                                InputProps={{
-                                    startAdornment: (
-                                        <Lock size="20" color="#B03E9F" />
-                                    ),
-                                }}
-                                inputProps={{
-                                    style: inputStyle,
-                                }}
-                            />
-                        </Box>
-                        <Box mt={2}>
-                            <ButtonBase
-                                className={classes.forgotPasswordButton}
-                                onClick={handleForgotPassword}
+                            Log in
+                        </Typography>
+                        <FormControl fullWidth className={classes.form}>
+                            <Box
+                                display="flex"
+                                flexDirection="column"
+                                marginTop={3}
                             >
-                                Esqueceu a senha?
-                            </ButtonBase>
-                            <LogInButton
-                                type="submit"
-                                mt={60}
-                                onClick={handleSubmit}
-                            />
-                        </Box>
-                    </FormControl>
-                </Box>
-                <Box
-                    display="flex"
-                    flexDirection="row"
-                    alignItems="center"
-                    mt={8}
-                >
-                    <Typography className={classes.signUpText}>
-                        Não possui uma conta?
-                    </Typography>
-                    <ButtonBase
-                        className={classes.signUpButton}
-                        onClick={() => console.log('New Account')}
+                                <TextField
+                                    id="outlined-basic"
+                                    label="Email"
+                                    variant="outlined"
+                                    InputProps={{
+                                        startAdornment: (
+                                            <UserTie
+                                                size="20"
+                                                color="#B03E9F"
+                                            />
+                                        ),
+                                    }}
+                                    inputProps={{
+                                        style: inputStyle,
+                                    }}
+                                />
+                                <TextField
+                                    id="outlined-basic"
+                                    type="password"
+                                    label="Senha"
+                                    variant="outlined"
+                                    className={classes.input}
+                                    InputProps={{
+                                        startAdornment: (
+                                            <Lock size="20" color="#B03E9F" />
+                                        ),
+                                    }}
+                                    inputProps={{
+                                        style: inputStyle,
+                                    }}
+                                />
+                            </Box>
+                            <Box mt={2}>
+                                <Link
+                                    to="/esqueci-a-senha"
+                                    className={classes.forgotPasswordButton}
+                                >
+                                    Esqueceu a senha?
+                                </Link>
+                                <LogInButton
+                                    type="submit"
+                                    mt={60}
+                                    text="Entrar"
+                                    onClick={handleSubmit}
+                                />
+                            </Box>
+                        </FormControl>
+                    </Box>
+                    <Box
+                        display="flex"
+                        flexDirection="row"
+                        alignItems="center"
+                        mt={8}
                     >
-                        Cadastre-se
-                    </ButtonBase>
-                </Box>
-            </Grid>
-            <Grid item xs={12} sm={6} className={classes.rightSide}>
-                <Typography style={{ fontFamily: 'Roboto', color: '#7A7A7A' }}>
-                    Ou acesse usando uma das suas contas:
-                </Typography>
-                <Box className={classes.divider} />
-                {/* <Divider className={classes.divider} /> */}
-                <Grid
-                    container
-                    spacing={5}
-                    alignItems="center"
-                    justify="center"
-                    direction="column"
-                >
-                    <Grid item xs>
-                        <CompanyButton icon={googleIcon} company={'Google'} />
-                    </Grid>
-                    <Grid item xs>
-                        <CompanyButton
-                            icon={microsoftIcon}
-                            company={'Microsoft'}
-                        />
-                    </Grid>
-                    <Grid item xs>
-                        <CompanyButton icon={appleIcon} company={'Apple'} />
+                        <Typography className={classes.signUpText}>
+                            Não possui uma conta?
+                        </Typography>
+                        <ButtonBase
+                            className={classes.signUpButton}
+                            onClick={() => console.log('New Account')}
+                        >
+                            Cadastre-se
+                        </ButtonBase>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} sm={6} className={classes.rightSide}>
+                    <Typography
+                        style={{ fontFamily: 'Roboto', color: '#7A7A7A' }}
+                    >
+                        Ou acesse usando uma das suas contas:
+                    </Typography>
+                    <Box className={classes.divider} />
+                    {/* <Divider className={classes.divider} /> */}
+                    <Grid
+                        container
+                        spacing={5}
+                        alignItems="center"
+                        justify="center"
+                        direction="column"
+                    >
+                        <Grid item xs>
+                            <CompanyButton
+                                icon={googleIcon}
+                                company={'Google'}
+                            />
+                        </Grid>
+                        <Grid item xs>
+                            <CompanyButton
+                                icon={microsoftIcon}
+                                company={'Microsoft'}
+                            />
+                        </Grid>
+                        <Grid item xs>
+                            <CompanyButton icon={appleIcon} company={'Apple'} />
+                        </Grid>
                     </Grid>
                 </Grid>
             </Grid>
-        </Grid>
+        </AuthLayout>
     );
-};
-
-const useStyles = makeStyles(theme => ({
-    container: { padding: '5% 10% 0px' },
-
-    form: {
-        paddingRight: '15%',
-    },
-
-    rightSide: {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-
-    input: {
-        marginTop: 20,
-    },
-    forgotPasswordButton: {
-        color: '#515151',
-        fontFamily: 'Roboto',
-        fontSize: 16,
-        fontWeight: 300,
-
-        transition: 'all .2s',
-
-        '&:hover': {
-            color: theme.palette.primary.main,
-            textDecoration: 'underline',
-        },
-    },
-    signUpText: {
-        font: 'italic 300 16px Roboto',
-        color: '#8A8A8A',
-    },
-
-    signUpButton: {
-        marginLeft: 5,
-
-        color: '#555',
-        // fontSize: 16,
-        font: '16px Roboto Slab',
-
-        '&:hover': {
-            color: theme.palette.primary.main,
-            textDecoration: 'underline',
-        },
-    },
-
-    divider: {
-        margin: theme.spacing(2, 0),
-        width: '60%',
-        border: '1px solid gray',
-        opacity: 0.2,
-    },
-}));
-
-const inputStyle = {
-    paddingLeft: 5,
-    fontFamily: 'Roboto',
 };
 
 export default Login;

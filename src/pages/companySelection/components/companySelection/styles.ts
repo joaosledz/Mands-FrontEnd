3e34983c99496565ef6cb5741709e414cc459c6b
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-09-21 09:56:27 -0300

    Resolução parcial do Scroll Horizontal desnecessário.

--- src/pages/companySelection/components/companySelection/styles.ts
+++ src/pages/companySelection/components/companySelection/styles.ts
@@ -3,12 +3,12 @@ import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         container: {
-            paddingTop: '5%',
+            padding: '2rem 0',
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
             // Tirar o overflow e resolver
-            overflow: 'hidden',
+            overflowX: 'hidden',
         },
 
         title: {

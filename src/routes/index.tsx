be979c0680112e51719623d8b23719f5df05614c
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-09-24 08:00:17 -0300

    Alteração do Router p/ BrowserRouter nas rotas da aplicação.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Switch, Route, Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 // import PrivateRoute from './privateRoute';
 
@@ -19,36 +18,29 @@ import CompanyDashboard from '../pages/companyDashboard';
 import DepartmentsAdminDashboard from '../pages/adminDashboard/departments';
 //#endregion
 
-const Routes = () => {
-    const browserHistory = createBrowserHistory();
-
-    return (
-        <Router history={browserHistory}>
-            <Switch>
-                {/* Transformar as rotas abaixo em PublicRoutes */}
-                {/* Rotas de Autenticação */}
-                <Route path="/" component={Login} exact />
-                <Route path="/criar-conta" component={Register} />
-                <Route path="/esqueci-a-senha" component={ForgotPassword} />
-                {/* Transformar as rotas abaixo em PrivateRoute */}
-                <Route
-                    path="/escolha-da-empresa"
-                    component={CompanySelection}
-                />
-                <Route
-                    path="/dashboard/:companyName"
-                    component={CompanyDashboard}
-                />
-                {/* Rotas do Administrador */}
-                <Route
-                    path="/administrador/departamentos"
-                    component={DepartmentsAdminDashboard}
-                />
-            </Switch>
-            {/* <PrivateRoute path="/escolha-empresa" component={CompanySelection} /> */}
-            {/* <PrivateRoute path="/user" component={User} /> */}
-        </Router>
-    );
-};
+const Routes = () => (
+    <BrowserRouter>
+        <Switch>
+            {/* Transformar as rotas abaixo em PublicRoutes */}
+            {/* Rotas de Autenticação */}
+            <Route path="/" component={Login} exact />
+            <Route path="/criar-conta" component={Register} />
+            <Route path="/esqueci-a-senha" component={ForgotPassword} />
+            {/* Transformar as rotas abaixo em PrivateRoute */}
+            <Route path="/escolha-da-empresa" component={CompanySelection} />
+            <Route
+                path="/dashboard/:companyName"
+                component={CompanyDashboard}
+            />
+            {/* Rotas do Administrador */}
+            <Route
+                path="/administrador/departamentos"
+                component={DepartmentsAdminDashboard}
+            />
+        </Switch>
+        {/* <PrivateRoute path="/escolha-empresa" component={CompanySelection} /> */}
+        {/* <PrivateRoute path="/user" component={User} /> */}
+    </BrowserRouter>
+);
 
 export default Routes;

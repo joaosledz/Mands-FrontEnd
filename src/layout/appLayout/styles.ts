03f2fbb225abfbbff31e15c105d1d157f8a7f27e
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-09-25 16:08:55 -0300

    Ajustes da altura dos Layouts

--- src/layout/appLayout/styles.ts
+++ src/layout/appLayout/styles.ts
@@ -3,9 +3,9 @@ import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         layout: {
-            flex: 1,
             width: '100vw',
             height: '100%',
+            minHeight: '100vh',
             maxWidth: '100%',
             backgroundColor: theme.palette.primary.light,
         },

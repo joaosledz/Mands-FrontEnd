16f65ddbbfe4669991331f3f6e270b2438b70888
victor0096 <victor.bulhoes@gmail.com>
2020-09-29 17:41:01 -0300

    Espaçamento e centralização do userInfo

--- src/pages/userProfile/main/components/userInfo/index.tsx
+++ src/pages/userProfile/main/components/userInfo/index.tsx
@@ -17,11 +17,18 @@ const User: React.FC<Props> = ({ name, role, email, telephone }) => {
     const classes = useStyles();
 
     return (
-        <Grid container direction="row" justify="center">
-            <Grid item>
+        <Grid container direction="row" justify="center" spacing={2}>
+            <Grid container item justify="center" xs={12} sm={4}>
                 <Avatar className={classes.largeAvatar} src={avatar} />
             </Grid>
-            <Grid item className={classes.UserDescriptionDiv}>
+            <Grid
+                container
+                item
+                className={classes.UserDescriptionDiv}
+                spacing={1}
+                xs={12}
+                sm={8}
+            >
                 <Typography variant="h6" className={classes.subtitle2}>
                     {name}
                 </Typography>

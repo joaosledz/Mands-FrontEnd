eb87404d15201cd93e5b29dcd0b0d01ec02f3405
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-10-05 17:08:27 -0300

    Ajustes na estilização do 'mainButton'

--- src/components/mainButton/styles.ts
+++ src/components/mainButton/styles.ts
@@ -4,34 +4,31 @@ type ButtonProps = {
     mt?: number;
     mw?: number;
     mwt?: number;
-    text: string;
-    icon?: string;
 };
 
 const useStyles = makeStyles<Theme, ButtonProps>(theme =>
     createStyles({
         button: {
             width: '100%',
-            maxWidth: props => (props.mw ? props.mw : 400),
+            height: 50,
+            maxWidth: props => (props.mw ? props.mw : 260),
             marginTop: props => props.mt,
             padding: '10px',
 
-            display: 'flex',
-            alignItems: 'center',
-            overflow: 'hidden',
-            height: 60,
             color: 'white',
-            textAlign: 'center',
-
             borderRadius: '7px',
             background:
                 'linear-gradient(90deg, rgba(210,91,197,1) 0%, rgba(197,71,150,1) 48%)',
 
-            flex: 1,
-
+            display: 'flex',
+            alignItems: 'center',
+            textAlign: 'center',
             justifyContent: 'center',
+
+            transition: 'all .2s',
+
             '&:hover': {
-                maxWidth: props => (props.mwt ? props.mwt : 360),
+                maxWidth: props => (props.mwt ? props.mwt : 300),
                 background: `linear-gradient(to bottom,${theme.palette.primary.main} ,${theme.palette.primary.main})`,
             },
         },
@@ -39,7 +36,6 @@ const useStyles = makeStyles<Theme, ButtonProps>(theme =>
         buttonText: {
             fontSize: 16,
             letterSpacing: 2.5,
-            // fontWeight: 500,
         },
     })
 );

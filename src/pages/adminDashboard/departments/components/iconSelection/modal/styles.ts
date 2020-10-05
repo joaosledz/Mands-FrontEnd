8a4c93535b84d5c59fd9ae0aeb89d789971cfdf1
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-10-05 17:11:30 -0300

    Implementação do componente de seleção de ícones

---
+++ src/pages/adminDashboard/departments/components/iconSelection/modal/styles.ts
@@ -0,0 +1,41 @@
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles<Theme>(theme =>
+    createStyles({
+        paper: {
+            width: '100%',
+            maxWidth: '50vw',
+            paddingTop: '2rem',
+
+            boxShadow: theme.shadows[5],
+
+            position: 'fixed',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            left: 0,
+            right: 0,
+            textAlign: 'center',
+
+            '& h1#iconModal-title': {
+                fontSize: '1.7rem',
+                fontWeight: 500,
+                color: theme.palette.primary.main,
+            },
+        },
+
+        iconsGridContainer: {
+            // marginTop: '2rem',
+            padding: '2rem',
+        },
+
+        iconGridItem: {
+            boxShadow: theme.shadows[2],
+            transition: 'background-color .2s',
+            '&:hover': {
+                backgroundColor: theme.palette.primary.light,
+            },
+        },
+    })
+);
+
+export default useStyles;

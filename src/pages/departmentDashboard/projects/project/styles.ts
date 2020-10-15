ce5e6a205b730f199e2b822ad333609ee3e8760c
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-10-15 09:16:05 -0300

    Construção do Grid de Projetos

---
+++ src/pages/departmentDashboard/projects/project/styles.ts
@@ -0,0 +1,41 @@
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        project: {
+            maxWidth: '12rem',
+            padding: '1rem',
+
+            textDecoration: 'none',
+
+            borderRadius: 10,
+            backgroundColor: theme.palette.primary.main,
+            boxShadow: theme.shadows[2],
+
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexWrap: 'initial',
+
+            transition: 'all .1s',
+
+            '& img': {
+                maxWidth: '80%',
+            },
+
+            '& p': {
+                marginTop: '0.5rem',
+                color: theme.palette.primary.contrastText,
+                fontSize: '1.1rem',
+                fontWeight: 300,
+            },
+
+            '&:hover': {
+                backgroundColor: theme.palette.primary.dark,
+            },
+        },
+    })
+);
+
+export default useStyles;
---
+++ src/pages/departmentDashboard/projects/styles.ts
@@ -0,0 +1,30 @@
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        container: {
+            padding: '1.5rem 5%',
+            backgroundColor: theme.palette.background.default,
+            [theme.breakpoints.only('xs')]: {
+                padding: '1.5rem 1%',
+            },
+        },
+
+        title: {
+            color: theme.palette.text.secondary,
+            fontSize: '1.3rem',
+            fontWeight: 300,
+            [theme.breakpoints.only('xs')]: {
+                marginLeft: '1rem'
+            },
+        },
+
+        projectsContainer: {
+            marginTop: '1rem',
+            maxHeight: 300,
+            overflow: 'auto',
+        },
+    })
+);
+
+export default useStyles;

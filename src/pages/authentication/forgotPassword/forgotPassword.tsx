a47854c93ec80300896320448f815e2d3a53c14b
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-10-19 10:26:43 -0300

    Ajustes de estilização do 'ForgotPassword'

--- src/pages/authentication/forgotPassword/forgotPassword.tsx
+++ src/pages/authentication/forgotPassword/forgotPassword.tsx
@@ -1,7 +1,7 @@
 import React, { FormEvent } from 'react';
-import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import FormControl from '@material-ui/core/FormControl';
+import Hidden from '@material-ui/core/Hidden';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 
@@ -22,42 +22,46 @@ const ForgotPassword: React.FC = () => {
     return (
         <AuthLayout backButtonMessage="Voltar para o Login">
             <Grid container>
-                <Grid item xs={12} sm={7}>
-                    <Box mt={4}>
-                        <Typography className={classes.title} color="primary">
-                            Esqueceu sua senha?
-                        </Typography>
-                        <Typography className={classes.description}>
-                            Digite o seu email abaixo. Enviarmos um link para
-                            que você possa resetar sua senha.
-                        </Typography>
-                        <FormControl fullWidth className={classes.form}>
-                            <TextField
-                                id="outlined-basic"
-                                label="Email"
-                                variant="outlined"
-                                inputProps={{
-                                    style: inputStyle,
-                                }}
-                            />
-                            <SendEmailButton
-                                mt={60}
-                                mw={350}
-                                mwt={390}
-                                text="Enviar link de reset"
-                                icon="email"
-                                onClick={handleSubmit}
-                            />
-                        </FormControl>
-                    </Box>
+                <Grid item xs={12} sm={7} className={classes.formGrid}>
+                    <Typography className={classes.title} color="primary">
+                        Esqueceu sua senha?
+                    </Typography>
+                    <Typography className={classes.description}>
+                        Digite o seu email abaixo. Enviarmos um link para que
+                        você possa resetar sua senha.
+                    </Typography>
+                    <FormControl fullWidth className={classes.form}>
+                        <TextField
+                            id="outlined-basic"
+                            label="Email"
+                            variant="outlined"
+                            inputProps={{
+                                style: inputStyle,
+                            }}
+                        />
+                        <SendEmailButton
+                            mt={60}
+                            mw={350}
+                            mwt={390}
+                            text="Enviar link de reset"
+                            icon="email"
+                            onClick={handleSubmit}
+                        />
+                    </FormControl>
                 </Grid>
-                <Grid item xs={12} sm={5}>
-                    <img
-                        src={forgotPasswordIllustration}
-                        style={{ width: '80%', minWidth: 260, height: 'auto' }}
-                        alt="Ilustração de um homem com uma camisa roxa, com o dedo indicador no queixo, tentando lembrar a senha da conta"
-                    />
-                </Grid>
+                <Hidden smDown>
+                    <Grid item xs={12} sm={5}>
+                        <img
+                            src={forgotPasswordIllustration}
+                            style={{
+                                width: '80%',
+                                minWidth: 260,
+                                height: 'auto',
+                            }}
+                            alt="Ilustração de um homem, com o dedo indicador no queixo, tentando lembrar a senha da conta"
+                        />
+                    </Grid>
+                </Hidden>
             </Grid>
         </AuthLayout>
     );

1f3aadd952b6f79b38134a51eb8495a0c70326bc
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-10-21 11:49:31 -0300

    Mudanças no AuthContext
    + Adds:
    1. Implementação da verificação de token;
    2. Implementação do logout;
    
    + Refactories:
    1. Ajuste da função de Login p/ função CallBack

--- src/contexts/auth/index.tsx
+++ src/contexts/auth/index.tsx
@@ -1,6 +1,10 @@
-import React, { createContext, useState, useEffect, useContext } from 'react';
-
-// import ApiService, { SetUserId } from '../../services';
+import React, {
+    createContext,
+    useState,
+    useEffect,
+    useContext,
+    useCallback,
+} from 'react';
 import { api, auth, LoginType, LoginModel, userType } from '../../services';
 
 type AuthContextData = {
@@ -20,67 +24,54 @@ export const AuthProvider: React.FC = ({ children }) => {
     // const [haveLogged, setHaveLogged] = useState(true);
 
     useEffect(() => {
-        // setLoading(true);
+        setLoading(true);
         const loadStoragedData = () => {
             const storagedToken = localStorage.getItem('@Mands:token');
-
             if (storagedToken) {
-                // api.defaults.headers[
-                //     'Authorization'
-                // ] = `Bearer ${storagedToken}`;
-                // ApiService.GetUserData()
-                //     .then((response) => {
-                //         // console.log(response.data);
-                //         SetUserId(response.data.company.id);
-                //         setUser(response.data);
-                //         setLoading(false);
-                //     })
-                //     .catch((error) => {
-                //         // console.log(error);
-                //         setLoading(false);
-                //     });
+                api.defaults.headers[
+                    'Authorization'
+                ] = `Bearer ${storagedToken}`;
+                auth.me()
+                    .then(response => {
+                        // console.log(response);
+                        //         SetUserId(response.data.company.id);
+                        setUser(response.data);
+                        setLoading(false);
+                    })
+                    .catch(error => {
+                        // console.log(error);
+                        setLoading(false);
+                    });
             } else setLoading(false);
         };
         loadStoragedData();
     }, []);
 
-    const login = async (data: LoginType) => {
+    const login = useCallback(async (data: LoginType) => {
         setLoading(true);
         try {
             const response = await auth.login(data);
-            console.log(response);
+            // console.log(response);
             setUser(response.data.user);
             // SetUserId(res.data.company.id);
             api.defaults.headers[
                 'Authorization'
             ] = `Bearer ${response.data.token}`;
-            localStorage.setItem('@SeuZe:token', response.data.token);
+            localStorage.setItem('@Mands:token', response.data.token);
             setLoading(false);
-
             return Promise.resolve(response.data);
         } catch (error) {
-            console.log(error);
+            // console.log(error);
             setLoading(false);
             return Promise.reject(error);
         }
-    };
+    }, []);
 
-    const logout = () => {
-        // return ApiService.Logout(refreshToken)
-        //     .then((res) => {
-        //         // console.log(res);
-        //         localStorage.clear();
-        //         api.defaults.headers['Authorization'] = null;
-        //         // localStorage.removeItem('@SeuZe:refreshToken');
-        //         // localStorage.removeItem('@SeuZe:token');
-        //         // localStorage.removeItem('@SeuZe:user');
-        //         setUser(null);
-        //     })
-        //     .catch((error) => {
-        //         // console.log(error);
-        //         return Promise.reject(error);
-        //     });
-    };
+    const logout = useCallback(() => {
+        localStorage.clear();
+        api.defaults.headers['Authorization'] = null;
+        setUser(null);
+    }, []);
 
     return (
         <AuthContext.Provider
@@ -91,10 +82,7 @@ export const AuthProvider: React.FC = ({ children }) => {
     );
 };
 
-// export default AuthContext;
-
 export function useAuth() {
     const context = useContext(AuthContext);
-
     return context;
 }

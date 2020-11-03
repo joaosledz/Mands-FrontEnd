20bb5d5072c8dbdebb7fb42f677de004cf130409
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-11-03 17:29:18 -0300

    Adição da rota UserCompanies
    + Add: Adição do memo no componente Company

--- src/pages/companySelection/companySelection/company/index.tsx
+++ src/pages/companySelection/companySelection/company/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { useHistory } from 'react-router-dom';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 
-import CompanyType from '../../../../../models/company';
-import handleUrlParamName from '../../../../../utils/functions/handleUrlParamName';
+import { CompanyType } from '../../../../services';
+import handleUrlParamName from '../../../../utils/functions/handleUrlParamName';
 
 import useStyles from './styles';
 
@@ -17,7 +17,7 @@ const Company: React.FC<Props> = (props: Props) => {
     const classes = useStyles();
     const history = useHistory();
     const { company } = props;
-    const { logo, name } = company;
+    const { image, name } = company;
 
     const handleCompanySelection = (companyData: CompanyType) => {
         sessionStorage.setItem(
@@ -33,7 +33,7 @@ const Company: React.FC<Props> = (props: Props) => {
             className={classes.container}
         >
             <Avatar
-                src={logo}
+                src={image}
                 alt={`${name} logo`}
                 className={classes.companyLogo}
             />
@@ -42,4 +42,4 @@ const Company: React.FC<Props> = (props: Props) => {
     );
 };
 
-export default Company;
+export default memo(Company);

b3840a2a1bed7eb1f7628fe6a5ff3406aa330cb0
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-11-11 11:28:30 -0300

    Implementação inicial do service de permissões
    + Add:
    1. Implementação da requisição de Permissões do usuário na empresa

---
+++ src/services/models/companyPermission.ts
@@ -0,0 +1,13 @@
+export type TypeCompanyPermission = {
+    compPermissionId: number;
+    name: string;
+    acceptUser: boolean;
+    deleteUser: boolean;
+    department: boolean;
+    project: boolean;
+    editCompany: boolean;
+    resetPIN: boolean;
+    seePIN: boolean;
+    event: boolean;
+    permission: boolean;
+};

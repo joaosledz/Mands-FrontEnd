987f4029b2083a760f57681cbc1596209db861a0
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-11-11 16:39:56 -0300

    Finalização do CompanyDetails/Header
    + Adds:
    1. Adição do botão para criação de empresa
    2. Implementação do sistema de troca de empresa

--- src/pages/companyDashboard/components/companyDetails/header/header.tsx
+++ src/pages/companyDashboard/components/companyDetails/header/header.tsx
@@ -1,26 +1,32 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory, Link } from 'react-router-dom';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 import Avatar from '@material-ui/core/Avatar';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import { ChevronDown as ChevronDownIcon } from '@styled-icons/entypo';
+import { Plus as PlusIcon } from '@styled-icons/entypo';
 
-import { CompanyType, companyApi } from '../../../../../services';
+import { UserCompanyType, companyApi } from '../../../../../services';
 
 import ITLogo from '../../../../../assets/fakeDataImages/companiesImages/IT2.png';
 import useStyles from './styles';
 
 type Props = {
-    company: CompanyType;
+    company: UserCompanyType;
 };
 
 const Header: React.FC<Props> = ({ company }) => {
     const classes = useStyles();
+    const history = useHistory();
 
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-    const [companies, setCompanies] = useState<Array<CompanyType> | null>(null);
+    const [companies, setCompanies] = useState<Array<UserCompanyType> | null>(
+        null
+    );
 
     useEffect(() => {
         const getAllCompanies = async () => {
@@ -36,6 +42,10 @@ const Header: React.FC<Props> = ({ company }) => {
 
     const handleOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
         setAnchorEl(event.currentTarget);
+    };
+
+    const handleChangeCompany = (company_username: string) => {
+        history.replace(`/dashboard/${company_username}`);
     };
 
     const handleClose = () => {
@@ -64,10 +74,24 @@ const Header: React.FC<Props> = ({ company }) => {
             >
                 <MenuItem disabled>Trocar de empresa:</MenuItem>
                 {companies?.map(company => (
-                    <MenuItem key={company.companyId} onClick={handleClose}>
+                    <MenuItem
+                        key={company.companyId}
+                        onClick={() => handleChangeCompany(company.username)}
+                    >
                         {company.name}
                     </MenuItem>
                 ))}
+                <MenuItem disabled>
+                    <Divider variant="middle" />
+                </MenuItem>
+                <MenuItem
+                    component={Link}
+                    to="/cadastrar-empresa"
+                    className={classes.registerCompany}
+                >
+                    <PlusIcon size={16} />{' '}
+                    <Typography>Criar nova empresa</Typography>
+                </MenuItem>
             </Menu>
         </Box>
     );

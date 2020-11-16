e28fb5c3e1fee4c7bb408389c21609fe50196d04
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-11-16 19:38:24 -0300

    Ajuste no departmentDashboard/header

--- src/pages/departmentDashboard/header/header.tsx
+++ src/pages/departmentDashboard/header/header.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
+import TypeParams from '../../../models/params';
 import BackButton from '../../../components/backButton';
 import useStyles from './styles';
 
@@ -11,6 +13,7 @@ type Props = {
 
 const CompanyDashboardHeader: React.FC<Props> = (props: Props) => {
     const classes = useStyles();
+    const params = useParams<TypeParams>();
     const { jobTitle } = props;
 
     return (
@@ -19,7 +22,11 @@ const CompanyDashboardHeader: React.FC<Props> = (props: Props) => {
                 <Typography className={classes.jobTitle}>{jobTitle}</Typography>
             </Grid>
             <Grid container item justify="flex-end" xs={6}>
-                <BackButton message="Voltar" white />
+                <BackButton
+                    message="Voltar"
+                    white
+                    redirect={`dashboard/${params.companyName}`}
+                />
             </Grid>
         </Grid>
     );

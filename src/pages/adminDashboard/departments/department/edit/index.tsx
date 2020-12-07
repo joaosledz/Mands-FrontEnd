99d67144cc9d347f8b5298a98db30c9e3acb4bce
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-12-07 12:40:41 -0300

    Integração parcial da edição de departamento

--- src/pages/adminDashboard/departments/department/edit/index.tsx
+++ src/pages/adminDashboard/departments/department/edit/index.tsx
@@ -1,126 +1,203 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory, useParams } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
+import InputMask from 'react-input-mask';
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
 
-import { ApiProps as DepartmentProps } from '../../../../../models/department';
-import departments from '../../../../../utils/data/departments';
+import {
+    TypeDepartment,
+    DepartmentModel,
+    departmentApi,
+} from '../../../../../services';
+import TypeParams from '../../../../../models/params';
+import useDepartment from '../../../../../hooks/useDepartment';
+import useCompany from '../../../../../hooks/useCompany';
+import snackbarUtils from '../../../../../utils/functions/snackbarUtils';
 
-import AppLayout from '../../../../../layout/appLayout';
+import AdminLayout from '../../../layout/layout';
 import SubmitButton from '../../../../../components/mainButton';
 import IconSelectionInput from '../../components/iconSelection/input';
 import Header from '../../components/header/header';
+import ErrorContent from '../../../../../components/errorContent/errorContent';
 import useStyles from './styles';
 
 const Edit: React.FC = () => {
     const classes = useStyles();
-    const location = useLocation<DepartmentProps>();
+    const location = useLocation<TypeDepartment>();
+    const history = useHistory();
+    const params = useParams<TypeParams>();
+    const { register, errors, handleSubmit, formState } = useForm<
+        DepartmentModel
+    >();
+    const { company } = useCompany();
+    const { department: departmentData, updateDepartment } = useDepartment();
 
     const handleDepartmentData = () => {
-        return location.state ? location.state : departments[0];
+        console.log(location.state ? 'location' : 'dep');
+        return location.state ? location.state : departmentData;
     };
+
     const departmentState = handleDepartmentData();
-    const [department, setDepartment] = useState<DepartmentProps>(
-        departmentState
-    );
+    const [department] = useState(departmentState);
     const [departmentChanged, setDepartmentChanged] = useState<boolean>(false);
-    const [image, setImage] = useState<string | undefined>(department.icon);
+    const [image, setImage] = useState<string | undefined>(
+        departmentState?.image
+    );
 
     useEffect(() => {
-        document.title = `${departmentState.name} - Edição`;
-    }, [departmentState.name]);
+        if (departmentState) document.title = `${departmentState.name}/edição`;
+    }, [departmentState]);
 
     useEffect(() => {
         const checkDepartmentChanged = () => {
-            if (
-                JSON.stringify(departmentState) !==
-                    JSON.stringify(department) ||
-                departmentState.icon !== image
-            )
-                setDepartmentChanged(true);
-            else setDepartmentChanged(false);
+            setDepartmentChanged(formState.isDirty);
         };
         checkDepartmentChanged();
-    }, [department, departmentState, image]);
+    }, [formState]);
 
-    const handleChange = (
-        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-        key: string
-    ) => {
-        setDepartment({
-            ...department,
-            [key]: event.target.value,
-        });
+    const formSubmit = async (data: DepartmentModel) => {
+        try {
+            const response = await departmentApi.update(
+                department!.departmentId,
+                company!.companyId,
+                data
+            );
+            updateDepartment(response.data);
+            snackbarUtils.success('Departamento editado com sucesso');
+            history.push(
+                `/admin/${params.company}/departamentos/${params.department!}`
+            );
+        } catch (error) {
+            snackbarUtils.error(error.message);
+        }
     };
 
     return (
-        <AppLayout>
-            <Paper className={classes.container}>
-                <Header
-                    departmentName={departmentState.name}
-                    message="Cancelar Edição"
-                />
-                <Grid container spacing={3} className={classes.formContainer}>
-                    <Grid item xs={12} md={2}>
-                        <IconSelectionInput image={image} setImage={setImage} />
-                    </Grid>
-                    <Grid container item xs={12} md={6} spacing={3}>
-                        <Grid item xs={12} md={6}>
-                            <TextField
-                                fullWidth
-                                variant="outlined"
-                                label="Nome"
-                                value={department.name}
-                                onChange={event => handleChange(event, 'name')}
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={6}>
-                            <TextField
-                                fullWidth
-                                disabled
-                                variant="outlined"
-                                label="Gerente"
-                                value="Ana Tartari"
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={12}>
-                            <TextField
-                                fullWidth
-                                variant="outlined"
-                                label="Email"
-                                value={department.email}
-                                onChange={event => handleChange(event, 'email')}
-                            />
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                        <TextField
-                            fullWidth
-                            multiline
-                            rows={6}
-                            variant="outlined"
-                            label="Descrição"
-                            value={department.description}
-                            onChange={event =>
-                                handleChange(event, 'description')
-                            }
-                        />
-                    </Grid>
-                </Grid>
-                <Grid
-                    container
-                    justify="center"
-                    className={classes.submitButtonContainer}
-                >
-                    <SubmitButton
-                        text="Salvar alterações"
-                        disabled={!departmentChanged}
-                        onClick={() => console.log(!departmentChanged)}
+        <AdminLayout>
+            {department && (
+                <Paper className={classes.container}>
+                    <Header
+                        departmentName={departmentState!.name}
+                        message="Cancelar edição"
+                        page="edit"
                     />
-                </Grid>
-            </Paper>
-        </AppLayout>
+                    <Grid
+                        component="form"
+                        container
+                        spacing={3}
+                        className={classes.formContainer}
+                        onSubmit={handleSubmit(formSubmit)}
+                    >
+                        <Grid item xs={12} md={2}>
+                            <IconSelectionInput
+                                image={image}
+                                setImage={setImage}
+                            />
+                        </Grid>
+                        <Grid container item xs={12} md={6} spacing={3}>
+                            <Grid item xs={12} md={6}>
+                                <TextField
+                                    data-cy="department-name"
+                                    name="name"
+                                    label="Nome"
+                                    defaultValue={department.name}
+                                    inputRef={register({
+                                        required: 'Este campo é obrigatório',
+                                    })}
+                                />
+                                <ErrorMessage
+                                    errors={errors}
+                                    name="name"
+                                    render={({ message }) => (
+                                        <ErrorContent message={message} />
+                                    )}
+                                />
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <InputMask
+                                    mask={'(99) 99999-9999'}
+                                    maskChar="_"
+                                >
+                                    {() => (
+                                        <TextField
+                                            data-cy="department-phone"
+                                            name="phone"
+                                            label="Telefone"
+                                            defaultValue={department.phone}
+                                            inputRef={register({
+                                                minLength: {
+                                                    value: 15,
+                                                    message:
+                                                        'O número está incompleto',
+                                                },
+                                            })}
+                                        />
+                                    )}
+                                </InputMask>
+
+                                <ErrorMessage
+                                    errors={errors}
+                                    name="name"
+                                    render={({ message }) => (
+                                        <ErrorContent message={message} />
+                                    )}
+                                />
+                            </Grid>
+                            <Grid item xs={12} md={12}>
+                                <TextField
+                                    data-cy="department-email"
+                                    name="email"
+                                    label="Email"
+                                    defaultValue={department.email}
+                                    inputRef={register({
+                                        required: 'Esse campo é obrigatório',
+                                        pattern: {
+                                            // eslint-disable-next-line
+                                            value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                                            message:
+                                                'Deve seguir o formato nome@email.com',
+                                        },
+                                    })}
+                                />
+                                <ErrorMessage
+                                    errors={errors}
+                                    name="email"
+                                    render={({ message }) => (
+                                        <ErrorContent message={message} />
+                                    )}
+                                />
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={12} md={4}>
+                            <TextField
+                                multiline
+                                rows={6}
+                                name="objective"
+                                label="Descrição"
+                                defaultValue={department.objective}
+                                inputRef={register({
+                                    required: 'Esse campo é obrigatório',
+                                })}
+                            />
+                        </Grid>
+                        <Grid
+                            container
+                            justify="center"
+                            className={classes.submitButtonContainer}
+                        >
+                            <SubmitButton
+                                type="submit"
+                                disabled={!departmentChanged}
+                                text="Salvar alterações"
+                            />
+                        </Grid>
+                    </Grid>
+                </Paper>
+            )}
+        </AdminLayout>
     );
 };
 

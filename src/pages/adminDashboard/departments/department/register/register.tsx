3c9a2c921588691f847244ac7a13391a2dc9b65e
victor0096 <victor.bulhoes@gmail.com>
2020-12-11 10:53:36 -0300

    Integração de criar departamento

--- src/pages/adminDashboard/departments/department/register/register.tsx
+++ src/pages/adminDashboard/departments/department/register/register.tsx
@@ -4,28 +4,43 @@ import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Hidden from '@material-ui/core/Hidden';
-
+import { DepartmentModel, departmentApi } from '../../../../../services';
 import AppLayout from '../../../../../layout/appLayout';
 import BackButton from '../../../../../components/backButton';
 import SubmitButton from '../../../../../components/mainButton';
 import IconSelection from '../../components/iconSelection/input';
 import useStyles from './styles';
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
 
 const NewDepartment: React.FC = () => {
     const classes = useStyles();
     const [image, setImage] = useState<string | undefined>('');
+    const { register, errors, handleSubmit } = useForm<DepartmentModel>({});
 
     useEffect(() => {
         document.title = 'Cadastrar Departamento';
     }, []);
 
-    const handleSubmit = () => {
-        const department = {
-            image,
-        };
-        console.log(department);
+    // const handleSubmit = () => {
+    //     const department = {
+    //         image,
+    //     };
+    //     console.log(department);
+    // };
+    const onSubmit = (data: DepartmentModel) => {
+        console.log(data);
+        console.log(image);
+        departmentApi
+            .create(data)
+            .then(response => {
+                console.log(response);
+                //sucess alert
+            })
+            .catch(error => {
+                //error alert
+            });
     };
-
     return (
         <AppLayout>
             <Paper className={classes.container}>
@@ -45,53 +60,71 @@ const NewDepartment: React.FC = () => {
                         />
                     </Grid>
                 </Grid>
-                <Grid container spacing={3} className={classes.formContainer}>
-                    <Grid item xs={12} md={2}>
-                        <IconSelection setImage={setImage} />
-                    </Grid>
-                    <Grid container item xs={12} md={6} spacing={3}>
-                        <Grid item xs={12} md={6}>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Grid
+                        container
+                        spacing={3}
+                        className={classes.formContainer}
+                    >
+                        <Grid item xs={12} md={2}>
+                            <IconSelection setImage={setImage} />
+                        </Grid>
+                        <Grid container item xs={12} md={6} spacing={3}>
+                            <Grid item xs={12} md={6}>
+                                <TextField
+                                    name="name"
+                                    fullWidth
+                                    label="Nome"
+                                    variant="outlined"
+                                    inputRef={register({
+                                        required: 'Esse campo é obrigatório',
+                                    })}
+                                />
+                            </Grid>
+                            <Grid item xs={12} md={6}>
+                                <TextField
+                                    fullWidth
+                                    disabled
+                                    label="Gerente"
+                                    value="Ana Tartari"
+                                    variant="outlined"
+                                />
+                            </Grid>
+                            <Grid item xs={12} md={12}>
+                                <TextField
+                                    name="email"
+                                    fullWidth
+                                    label="Email"
+                                    variant="outlined"
+                                    inputRef={register({
+                                        required: 'Esse campo é obrigatório',
+                                    })}
+                                />
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={12} md={4}>
                             <TextField
+                                name="objective"
                                 fullWidth
-                                label="Nome"
+                                multiline
+                                rows={5}
                                 variant="outlined"
+                                label="Descrição"
+                                inputRef={register({
+                                    required: 'Esse campo é obrigatório',
+                                })}
                             />
                         </Grid>
-                        <Grid item xs={12} md={6}>
-                            <TextField
-                                fullWidth
-                                disabled
-                                label="Gerente"
-                                value="Ana Tartari"
-                                variant="outlined"
-                            />
-                        </Grid>
-                        <Grid item xs={12} md={12}>
-                            <TextField
-                                fullWidth
-                                label="Email"
-                                variant="outlined"
-                            />
-                        </Grid>
                     </Grid>
-                    <Grid item xs={12} md={4}>
-                        <TextField
-                            fullWidth
-                            multiline
-                            rows={5}
-                            variant="outlined"
-                            label="Descrição"
-                        />
+                    <Grid
+                        container
+                        xs={12}
+                        justify="center"
+                        className={classes.submitButtonContainer}
+                    >
+                        <SubmitButton text="Cadastrar" />
                     </Grid>
-                </Grid>
-                <Grid
-                    container
-                    xs={12}
-                    justify="center"
-                    className={classes.submitButtonContainer}
-                >
-                    <SubmitButton text="Cadastrar" onClick={handleSubmit} />
-                </Grid>
+                </form>
             </Paper>
         </AppLayout>
     );

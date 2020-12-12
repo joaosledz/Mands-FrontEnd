5fb2a776d015edfdd77b694f00995d37e79c935c
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-12-11 22:42:24 -0300

    feat: Integração da edição de projeto

--- src/pages/adminDashboard/departments/projects/edit/edit.tsx
+++ src/pages/adminDashboard/departments/projects/edit/edit.tsx
@@ -1,64 +1,59 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Hidden from '@material-ui/core/Hidden';
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
 
-import { TypeProjects } from '../../../../../models/department';
-import departmentsData from '../../../../../utils/data/departments';
+import { ProjectModel, projectApi } from '../../../../../services';
+import useProject from '../../../../../hooks/useProject';
+import snackbarUtils from '../../../../../utils/functions/snackbarUtils';
 
-import AppLayout from '../../../../../layout/appLayout';
+import ProjectLayout from '../../../layout/projectLayout';
 import BackButton from '../../../../../components/backButton';
 import SubmitButton from '../../../../../components/mainButton';
 import CropImageInput from '../../../../../components/cropImage/cropImageInput';
+import ErrorContent from '../../../../../components/errorContent/errorContent';
 import useStyles from './styles';
+import useCompany from '../../../../../hooks/useCompany';
+import useDepartment from '../../../../../hooks/useDepartment';
 
 const Edit: React.FC = () => {
     const classes = useStyles();
-    const location = useLocation<TypeProjects>();
+    const { register, errors, handleSubmit, formState, reset } = useForm<
+        ProjectModel
+    >();
+    const { company } = useCompany();
+    const { department } = useDepartment();
+    const { project, updateProject } = useProject();
 
-    const handleProjectData = () => {
-        console.log(location);
-        return location.state ? location.state : departmentsData[0].projects[0];
-    };
-
-    const projectState = handleProjectData();
-    const [project, setProject] = useState<TypeProjects>(projectState);
-    const [projectChanged, setProjectChanged] = useState<boolean>(false);
     const [image, setImage] = useState<File | undefined>();
 
     useEffect(() => {
-        document.title = `${projectState.name} - Edição`;
-    }, [projectState.name]);
+        if (project) document.title = `${project.name} - Edição`;
+    }, [project]);
 
-    useEffect(() => {
-        const checkProjectChanged = () => {
-            if (JSON.stringify(projectState) !== JSON.stringify(project))
-                setProjectChanged(true);
-            else if (image) {
-                if (projectState.icon !== URL.createObjectURL!(image))
-                    setProjectChanged(true);
-                else setProjectChanged(false);
-            } else setProjectChanged(false);
-        };
-        checkProjectChanged();
-    }, [project, projectState, image]);
-
-    const handleChange = (
-        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-        key: string
-    ) => {
-        console.log(event.target.value);
-        setProject({
-            ...project,
-            [key]: event.target.value,
-        });
+    const formSubmit = async (data: ProjectModel) => {
+        try {
+            const response = await projectApi.update(
+                company!.companyId,
+                department!.departmentId,
+                project!.projectId,
+                data
+            );
+            updateProject(response.data);
+            reset();
+            snackbarUtils.success('Departamento editado com sucesso');
+        } catch (error) {
+            console.log(error);
+            snackbarUtils.error(error.message);
+        }
     };
 
     return (
-        <AppLayout>
+        <ProjectLayout>
             <Paper className={classes.container}>
                 <Grid container>
                     <Hidden mdDown>
@@ -66,18 +61,25 @@ const Edit: React.FC = () => {
                     </Hidden>
                     <Grid container item xs={12} md={4} justify="center">
                         <Typography variant="h1" className={classes.title}>
-                            {projectState.name} - Edição
+                            {project?.name} - Edição
                         </Typography>
                     </Grid>
                     <Grid container item xs={12} md={4} justify="flex-end">
                         <BackButton message="Voltar" />
                     </Grid>
                 </Grid>
-                <Grid container spacing={3} className={classes.formContainer}>
+
+                <Grid
+                    container
+                    spacing={3}
+                    className={classes.formContainer}
+                    component="form"
+                    onSubmit={handleSubmit(formSubmit)}
+                >
                     <Grid item xs={12} md={2}>
                         <CropImageInput
                             image={image}
-                            preview={project.icon}
+                            preview={project?.image}
                             setImage={setImage}
                             styles={classes.cropImage}
                         />
@@ -85,82 +87,110 @@ const Edit: React.FC = () => {
                     <Grid container item xs={12} md={6} spacing={3}>
                         <Grid item xs={12} md={6}>
                             <TextField
-                                fullWidth
-                                variant="outlined"
+                                data-cy="project-name"
+                                name="name"
                                 label="Nome"
-                                value={project.name}
-                                onChange={event => handleChange(event, 'name')}
+                                defaultValue={project?.name}
+                                inputRef={register({
+                                    required: 'Este campo é obrigatório',
+                                })}
+                            />
+                            <ErrorMessage
+                                errors={errors}
+                                name="name"
+                                render={({ message }) => (
+                                    <ErrorContent message={message} />
+                                )}
                             />
                         </Grid>
                         <Grid item xs={12} md={6}>
                             <TextField
-                                fullWidth
-                                variant="outlined"
+                                data-cy="project-budget"
+                                name="budget"
                                 label="Orçamento"
-                                value={project.budget}
-                                onChange={event =>
-                                    handleChange(event, 'budget')
-                                }
+                                defaultValue={project?.budget}
+                                inputRef={register()}
                             />
                         </Grid>
                         <Grid item xs={12} md={6}>
                             <TextField
+                                data-cy="project-initialDate"
                                 type="date"
-                                fullWidth
                                 label="Data inicial"
-                                variant="outlined"
+                                name="initialDate"
+                                defaultValue={
+                                    project?.initialDate.split('T')[0]
+                                }
+                                inputRef={register({
+                                    required: 'Este campo é obrigatório',
+                                })}
                                 InputLabelProps={{
                                     shrink: true,
                                 }}
-                                value={project.initialDate}
-                                onChange={event =>
-                                    handleChange(event, 'initialDate')
-                                }
+                            />
+                            <ErrorMessage
+                                errors={errors}
+                                name="initialDate"
+                                render={({ message }) => (
+                                    <ErrorContent message={message} />
+                                )}
                             />
                         </Grid>
                         <Grid item xs={12} md={6}>
                             <TextField
+                                data-cy="project-finalDate"
                                 type="date"
-                                fullWidth
                                 label="Data Final"
-                                variant="outlined"
+                                name="finalDate"
+                                defaultValue={project?.finalDate.split('T')[0]}
+                                inputRef={register({
+                                    required: 'Este campo é obrigatório',
+                                })}
                                 InputLabelProps={{
                                     shrink: true,
                                 }}
-                                value={project.finalDate}
-                                onChange={event =>
-                                    handleChange(event, 'finalDate')
-                                }
+                            />
+                            <ErrorMessage
+                                errors={errors}
+                                name="finalDate"
+                                render={({ message }) => (
+                                    <ErrorContent message={message} />
+                                )}
                             />
                         </Grid>
                     </Grid>
                     <Grid item xs={12} md={4}>
                         <TextField
-                            fullWidth
+                            data-cy="project-description"
+                            name="description"
+                            label="Descrição"
                             multiline
                             rows={5}
-                            variant="outlined"
-                            label="Descrição"
-                            value={project.description}
-                            onChange={event =>
-                                handleChange(event, 'description')
-                            }
+                            defaultValue={project?.description}
+                            inputRef={register()}
+                        />
+                        <ErrorMessage
+                            errors={errors}
+                            name="description"
+                            render={({ message }) => (
+                                <ErrorContent message={message} />
+                            )}
+                        />
+                    </Grid>
+                    <Grid
+                        container
+                        justify="center"
+                        className={classes.submitButtonContainer}
+                    >
+                        <SubmitButton
+                            type="submit"
+                            text="Salvar alterações"
+                            disabled={!formState.isDirty}
                         />
                     </Grid>
                 </Grid>
-                <Grid
-                    container
-                    justify="center"
-                    className={classes.submitButtonContainer}
-                >
-                    <SubmitButton
-                        text="Salvar alterações"
-                        disabled={!projectChanged}
-                        onClick={() => console.log(!projectChanged)}
-                    />
-                </Grid>
             </Paper>
-        </AppLayout>
+        </ProjectLayout>
     );
 };
 

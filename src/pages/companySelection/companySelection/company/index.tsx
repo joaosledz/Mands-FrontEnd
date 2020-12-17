fd8e034cad749c019e14bfe35256d05b70813db6
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-12-17 18:19:27 -0300

    Ajuste no Company Selection para atualizar a context

--- src/pages/companySelection/companySelection/company/index.tsx
+++ src/pages/companySelection/companySelection/company/index.tsx
@@ -3,9 +3,9 @@ import { useHistory } from 'react-router-dom';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
+import useCompany from '../../../../hooks/useCompany';
 
 import { UserCompanyType } from '../../../../services';
-
 import useStyles from './styles';
 
 type Props = {
@@ -15,11 +15,12 @@ type Props = {
 const Company: React.FC<Props> = (props: Props) => {
     const classes = useStyles();
     const history = useHistory();
+    const { updateCompany } = useCompany();
     const { company } = props;
     const { imagePath, name, username } = company;
-    console.log(company);
+
     const handleCompanySelection = () => {
-        sessionStorage.setItem('@Mands:CompanyData', JSON.stringify(company));
+        updateCompany(company);
         history.push(`/dashboard/${username}`);
     };
 

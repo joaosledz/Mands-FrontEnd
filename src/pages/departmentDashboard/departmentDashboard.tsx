ccaabbb486bf9fdd7085facb5734bbf95010de8f
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2021-01-14 13:23:59 -0300

    refactor: correção da busca de dados no DepartmentDashboard

--- src/pages/departmentDashboard/departmentDashboard.tsx
+++ src/pages/departmentDashboard/departmentDashboard.tsx
@@ -4,7 +4,6 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 
 import TypeParams from '../../models/params';
-import useCompany from '../../hooks/useCompany';
 import useDepartment from '../../hooks/useDepartment';
 import snackbarUtils from '../../utils/functions/snackbarUtils';
 
@@ -17,58 +16,36 @@ import useStyles from './styles';
 const DepartmentDashboard: React.FC = () => {
     const classes = useStyles();
     const params = useParams<TypeParams>();
-    const { company, getCompanyData, loading, setLoading } = useCompany();
-    const { department, getDepartmentData } = useDepartment();
+    const { department, getDepartmentData, loading } = useDepartment();
 
     useEffect(() => {
-        const handleDepartmentParam = async () => {
-            setLoading(true);
-            try {
-                if (company && department) {
-                    document.title = `Departamento - ${department.name}`;
-                    if (
-                        params.department!.toLowerCase() !==
-                        department.name.toLowerCase()
-                    )
+        if (department) document.title = `Departamento - ${department.name}`;
+        else document.title = 'Carregando...';
+    }, [department]);
+
+    useEffect(() => {
+        const checkDepartmentData = async () => {
+            if (params.department) {
+                try {
+                    if (!department)
                         await getDepartmentData(
-                            company.username,
-                            department.name
+                            params.company,
+                            params.department
                         );
-                    // Fix: verificar bug na troca de departamento, pelas rotas
-                    else setLoading(false);
-                } else {
-                    if (company) {
-                        await getDepartmentData(
-                            company!.username,
-                            params.department!
-                        );
-                    } else {
-                        const response = await getCompanyData(params.company);
-                        await getDepartmentData(
-                            response.username,
-                            params.department!
-                        );
-                    }
+                } catch (error) {
+                    snackbarUtils.error(error.message);
                 }
-            } catch (error) {
-                snackbarUtils.error(error.message);
             }
         };
-        handleDepartmentParam();
-    }, [
-        company,
-        department,
-        params,
-        getCompanyData,
-        getDepartmentData,
-        setLoading,
-    ]);
+        checkDepartmentData();
+        // eslint-disable-next-line
+    }, []);
 
     return (
         <AppLayout loading={loading}>
-            {company && department && (
+            {department && (
                 <Box className={classes.container}>
-                    <Header jobTitle={company.userPermission!.name} />
+                    <Header jobTitle={'Gerente'} />
                     <Grid
                         container
                         spacing={3}

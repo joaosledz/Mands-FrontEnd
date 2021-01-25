e848e96d879d8568a52088f7968557812eefe8a7
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2021-01-25 20:16:40 -0300

    refactor: ajustes estruturais e de estilização do AssignTeamModal

--- src/pages/adminDashboard/components/assignTeamModal/department/roles/index.tsx
+++ src/pages/adminDashboard/components/assignTeamModal/department/roles/index.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormLabel from '@material-ui/core/FormLabel';
@@ -14,48 +15,54 @@ type Props = {
     roles: TypeDepartmentPermission[];
     roleValue: number;
     handleChangeRole: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    handleOpen: () => void;
 };
 
 const ChooseRole: React.FC<Props> = (props: Props) => {
     const classes = useStyles();
-    const { roles, roleValue, handleChangeRole } = props;
+    const { roles, roleValue, handleChangeRole, handleOpen } = props;
 
     return (
         <Fragment>
-            <FormLabel
-                component="legend"
-                style={{ paddingLeft: '21px', margin: '1rem 0rem' }}
-            >
-                Cargos
-            </FormLabel>
+            <Grid container>
+                <FormLabel
+                    component="legend"
+                    style={{ paddingLeft: '21px', margin: '1rem 0rem' }}
+                >
+                    Cargos
+                </FormLabel>
+            </Grid>
             {roles.length === 0 && (
-                <Typography>Este departamento não possui cargos</Typography>
-            )}
-            <RadioGroup
-                value={roleValue}
-                aria-label="cargos"
-                name="cargos"
-                onChange={handleChangeRole}
-            >
-                {roles.map(role => (
-                    <RoleItem key={role.depPermissionId} role={role} />
-                ))}
-                <Divider variant="fullWidth" className={classes.divider} />
-                <Grid container item className={classes.addRole}>
-                    <Grid item xs={1}>
-                        <AddIcon size={20} />
-                    </Grid>
-                    <Grid
-                        item
-                        xs={11}
-                        component={Typography}
-                        style={{ textAlign: 'left' }}
-                    >
-                        Adicionar cargo personalizado
-                    </Grid>
+                <Grid container component={Typography} justify="center">
+                    Este departamento não possui cargos
                 </Grid>
-                <Divider variant="fullWidth" className={classes.divider} />
-            </RadioGroup>
+            )}
+            <Grid container>
+                <RadioGroup
+                    value={roleValue}
+                    aria-label="cargos"
+                    name="cargos"
+                    onChange={handleChangeRole}
+                    style={{ width: '100%' }}
+                >
+                    {roles.map(role => (
+                        <RoleItem key={role.depPermissionId} role={role} />
+                    ))}
+                    <Divider variant="fullWidth" className={classes.divider} />
+                    <Grid
+                        container
+                        component={Button}
+                        onClick={handleOpen}
+                        className={classes.addRole}
+                    >
+                        <Grid item xs={1} component={AddIcon} size={20} />
+                        <Grid item xs={11} component={Typography}>
+                            Adicionar cargo personalizado
+                        </Grid>
+                    </Grid>
+                    <Divider variant="fullWidth" className={classes.divider} />
+                </RadioGroup>
+            </Grid>
         </Fragment>
     );
 };

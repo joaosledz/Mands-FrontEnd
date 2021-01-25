237945ba4655c20d17360b792a578749d1a94195
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2021-01-25 15:29:58 -0300

    refactor: ajuste no loading e tratamentos de empresa sem departamentos do Admin/Departments

--- src/pages/adminDashboard/departments/departments.tsx
+++ src/pages/adminDashboard/departments/departments.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect /* , useContext */ } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 
 import useCompany from '../../../hooks/useCompany';
-import { departmentApi } from '../../../services';
+import { departmentApi, TypeDepartment } from '../../../services';
 import snackbarUtils from '../../../utils/functions/snackbarUtils';
 
 import Layout from '../layout/departmentLayout';
@@ -14,7 +14,10 @@ import useStyles from './styles';
 const Departments: React.FC = () => {
     const classes = useStyles();
     const history = useHistory();
-    const { company, updateCompany, setLoading } = useCompany();
+    const { company } = useCompany();
+
+    const [loading, setLoading] = useState(true);
+    const [departments, setDepartments] = useState<TypeDepartment[]>([]);
 
     useEffect(() => {
         const fetchDepartments = async () => {
@@ -24,10 +27,7 @@ const Departments: React.FC = () => {
                     const response = await departmentApi.listByCompany(
                         company.companyId
                     );
-                    updateCompany({
-                        ...company,
-                        departments: [...response.data],
-                    });
+                    setDepartments(response.data);
                 } catch (error) {
                     snackbarUtils.error(error.message);
                 } finally {
@@ -36,8 +36,7 @@ const Departments: React.FC = () => {
             }
         };
         fetchDepartments();
-        // eslint-disable-next-line
-    }, []);
+    }, [company]);
 
     useEffect(() => {
         if (company) {
@@ -48,16 +47,11 @@ const Departments: React.FC = () => {
     return (
         <Layout title="Departamentos" menu>
             <Grid item xs={12} lg={9} className={classes.departmentsContainer}>
-                {company && (
-                    <React.Fragment>
-                        <DepartmentsContent
-                            departments={company.departments!}
-                            containerStyles={
-                                classes.departmentsContentContainer
-                            }
-                        />
-                    </React.Fragment>
-                )}
+                <DepartmentsContent
+                    departments={departments}
+                    containerStyles={classes.departmentsContentContainer}
+                    loading={loading}
+                />
             </Grid>
             <FabButton
                 onClick={() => history.push('departamentos/cadastrar')}

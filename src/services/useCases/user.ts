517be4eb85c8ca253ade918b12cd753d4f74f7d5
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2021-01-29 11:13:55 -0300

    refactor: correção da rota show

--- src/services/useCases/user.ts
+++ src/services/useCases/user.ts
@@ -1,19 +1,9 @@
 import api from '../api';
-import { AxiosResponse } from 'axios';
 import { TypeUser } from '../models/authentication';
 import userUrls from '../urls/user';
+
 const userApi = {
-    show: async (username: string) => {
-        try {
-            const response: AxiosResponse<TypeUser> = await api.get(
-                userUrls.show + username
-            );
-            return Promise.resolve(response);
-        } catch (error) {
-            console.log(error);
-            return Promise.reject(error);
-        }
-    },
+    show: (credential: string) => api.get<TypeUser>(userUrls.show + credential),
 };
 
 export default userApi;

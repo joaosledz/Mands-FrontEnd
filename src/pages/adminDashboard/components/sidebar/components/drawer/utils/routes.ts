ba71365d120a51630698dfc3d00bdf4b0e97b2b0
victor0096 <victor.bulhoes@gmail.com>
2021-02-16 12:08:52 -0300

    Base para listagem de projetos

--- src/pages/adminDashboard/components/sidebar/components/drawer/utils/routes.ts
+++ src/pages/adminDashboard/components/sidebar/components/drawer/utils/routes.ts
@@ -3,6 +3,7 @@ import { IRoutes } from '../models/iRoutes';
 import CompanyIcon from '../../../../../../../assets/icons/business.svg';
 import DepartmentIcon from '../../../../../../../assets/icons/department.svg';
 import EmployeesIcon from '../../../../../../../assets/icons/employees.svg';
+import ProjectIcon from '../../../../../../../assets/icons/project.svg';
 import CalendarIcon from '../../../../../../../assets/icons/calendar.svg';
 
 const Routes: IRoutes = [
@@ -23,6 +24,12 @@ const Routes: IRoutes = [
         icon: EmployeesIcon,
         iconAlt: 'Ícone de três pessoas',
         page: 'funcionarios',
+    },
+    {
+        name: 'Projetos',
+        icon: ProjectIcon,
+        iconAlt: 'Ícone de três pessoas',
+        page: 'projetos',
     },
     {
         name: 'Calendário',

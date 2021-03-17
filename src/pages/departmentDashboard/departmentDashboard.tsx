6a3cdcdaf1e097dc960d0de00ff640d1348b76d3
Marcos Marques <marcosmarquesdev@gmail.com>
2021-03-17 12:14:06 -0300

    Conserta erro de loading

--- src/pages/departmentDashboard/departmentDashboard.tsx
+++ src/pages/departmentDashboard/departmentDashboard.tsx
@@ -19,7 +19,12 @@ import useStyles from './styles';
 const DepartmentDashboard: React.FC = () => {
     const classes = useStyles();
     const params = useParams<TypeParams>();
-    const { department, getDepartmentData, loading } = useDepartment();
+    const {
+        department,
+        getDepartmentData,
+        loading,
+        setLoading,
+    } = useDepartment();
 
     const [notFound, setNotFound] = useState(false);
 
@@ -47,6 +52,8 @@ const DepartmentDashboard: React.FC = () => {
                             snackbarUtils.error(error.message);
                             break;
                     }
+                } finally {
+                    setLoading(false);
                 }
             }
         };
